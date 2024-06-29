0dc295e58fbd3a607321181b7899a49b2feefbf0
Pranav952 <pranavb952@gmail.com>
2024-06-29 21:55:17 +0545

    jazzmin admin panel

--- FoodApp/food/models.py
+++ FoodApp/food/models.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 
 # Create your models here.
 class Item(models.Model):
-    user_name=models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     item_name=models.CharField(max_length=100)
     item_desc=models.CharField(max_length=100)
     item_price=models.IntegerField()
