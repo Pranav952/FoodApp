7d76a6d6689c02643edf5c24d0c179ab37c1b0a6
Pranav952 <pranavb952@gmail.com>
2024-06-30 01:20:10 +0545

    general change

--- FoodApp/FoodApp/settings.py
+++ FoodApp/FoodApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7@(i(++amwm9t8mhj*l_xp$avqt717ore2vbn$noy#)0$vn$^d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
